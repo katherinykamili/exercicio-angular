a0642d91ba9e88113b71bce5e9c570105705a244
katherinykamili <katherinykc13@gmail.com>
2026-07-03 14:06:43 -0300

    feat: adiciona componente Compras com estrutura básica e testes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SobreMim } from "./sobre-mim/sobre-mim";
+import { UsuarioComponent } from "./usuario/usuario";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SobreMim],
+  imports: [RouterOutlet, SobreMim, UsuarioComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
